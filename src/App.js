1d14662a2881062962b7e16503439a5cb66f4723
Kacper Woloszyn <kacperwoloszyn4@gmail.com>
2017-10-03 18:14:55 +0100

    Static Version Completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+ import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+    class Form extends React.Component {
+      render() {
+        return (
+           <form style={{marginTop: '30px'}}>
+              <h3>Add a new post</h3>
 
-export default App;
+              <div className="form-group">
+                <input type="text"
+                  className="form-control"
+                  placeholder="Title"></input>
+              </div>
+              <div className="form-group">
+                <input type="text"
+                className="form-control"
+                placeholder="Link"></input>
+              </div>
+              <button type="submit" className="btn btn-primary">Post</button>
+            </form>
+            );
+        }
+    }
+
+    class NewsItem extends React.Component {
+      render() {
+          var divStyle = {
+               fontSize: '20px', 
+               marginLeft: '10px' 
+              };
+          var cursor = { cursor: 'pointer' } ;
+          var line ;
+          if (this.props.post.link ) {
+             line = <a href={this.props.post.link} >
+            {            this.props.post.title} </a> ;
+          } else {
+             line = <span>{this.props.post.title} </span> ;
+          }
+        return (
+            <div >
+              <span className="glyphicon glyphicon-thumbs-up"
+                    style={cursor} />
+              {this.props.post.upvotes}
+              <span style={divStyle} >{line}<span>
+                  <a href={'#/posts/' + this.props.post.id }>Comments</a>
+                </span>
+              </span>
+            </div>  
+            );
+      }
+    }
+
+    class NewsList extends React.Component {
+       render() {
+           var displayedPosts = this.props.posts.map(function(post) {
+            return <NewsItem key={post.id} post={post} /> ;
+          }) ;
+          return (
+                  <div className="col-md-10">
+                    <ul className="posts">
+                        {displayedPosts}
+                    </ul>
+                  </div>
+            ) ;
+       } 
+     }        
+
+    class HackerApp extends React.Component {
+      render() {
+          return (
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3">
+               <div className="page-header">
+                      <h1>Hacker News</h1>
+                      <NewsList posts={this.props.posts} />      
+                      <Form />
+               </div>
+                 </div>
+                </div>
+              </div>
+          );
+      }
+    }
+
+    export default HackerApp;
