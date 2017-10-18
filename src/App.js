fe8ffed159d4ca3b2a2fd19aca104d67e7fa7c07
Kacper Woloszyn <kacperwoloszyn4@gmail.com>
2017-10-18 23:40:10 +0100

    Hacker News- Upvote feature completed

--- src/App.js
+++ src/App.js
@@ -1,86 +1,99 @@
- import React from 'react';
+  import React from 'react';
+    import _ from 'lodash';
+    import api from './test/stubAPI';
 
     class Form extends React.Component {
-      render() {
-        return (
-           <form style={{marginTop: '30px'}}>
-              <h3>Add a new post</h3>
+        state = { title: '', link: ''};
 
-              <div className="form-group">
-                <input type="text"
-                  className="form-control"
-                  placeholder="Title"></input>
-              </div>
-              <div className="form-group">
-                <input type="text"
-                className="form-control"
-                placeholder="Link"></input>
-              </div>
-              <button type="submit" className="btn btn-primary">Post</button>
-            </form>
+        render() {
+           return (
+             <form style={{marginTop: '30px'}}>
+                <h3>Add a new post</h3>
+                <div className="form-group">
+                  <input type="text"
+                    className="form-control" placeholder="Title"
+                    value={this.state.title} ></input>
+                </div>
+                <div className="form-group">
+                  <input type="text"
+                     className="form-control" placeholder="Link"
+                     value={this.state.link} ></input>
+                </div>
+                <button type="submit" className="btn btn-primary" >Post</button>
+              </form>
             );
-        }
-    }
+          }
+       };
 
     class NewsItem extends React.Component {
+      handleVote = () =>  this.props.upvoteHandler(this.props.post.id);
+
       render() {
-          var divStyle = {
-               fontSize: '20px', 
-               marginLeft: '10px' 
-              };
-          var cursor = { cursor: 'pointer' } ;
-          var line ;
+          let lineStyle = {
+               fontSize: '20px', marginLeft: '10px'  };
+          let cursor = { cursor: 'pointer' } ;
+          let line ;
           if (this.props.post.link ) {
              line = <a href={this.props.post.link} >
-            {            this.props.post.title} </a> ;
+                          {this.props.post.title} </a> ;
           } else {
              line = <span>{this.props.post.title} </span> ;
           }
-        return (
+          return (
             <div >
-              <span className="glyphicon glyphicon-thumbs-up"
-                    style={cursor} />
+              <span className="glyphicon glyphicon-thumbs-up" 
+                  style={cursor} 
+                  onClick={this.handleVote} ></span>
               {this.props.post.upvotes}
-              <span style={divStyle} >{line}<span>
+              <span style={lineStyle} >{line}<span>
                   <a href={'#/posts/' + this.props.post.id }>Comments</a>
                 </span>
               </span>
             </div>  
             );
-      }
+        }
     }
 
     class NewsList extends React.Component {
-       render() {
-           var displayedPosts = this.props.posts.map(function(post) {
-            return <NewsItem key={post.id} post={post} /> ;
-          }) ;
-          return (
-                  <div className="col-md-10">
-                    <ul className="posts">
-                        {displayedPosts}
-                    </ul>
-                  </div>
-            ) ;
-       } 
-     }        
+      render() {
+          let items = this.props.posts.map((post,index) => {
+             return <NewsItem key={index} 
+                              post={post} 
+                              upvoteHandler={this.props.upvoteHandler}  /> ;
+            } )
+        return (
+           <div>
+              {items}
+           </div>
+          );
+      } 
+    }
 
     class HackerApp extends React.Component {
+      incrementUpvote = (id) => {
+          api.upvote(id) ;
+          this.setState({});
+      };
       render() {
+          let posts = _.sortBy(api.getAll(), function(post) {
+              return - post.upvotes;
+          }
+          );
           return (
               <div className="container">
-                <div className="row">
-                  <div className="col-md-6 col-md-offset-3">
-               <div className="page-header">
-                      <h1>Hacker News</h1>
-                      <NewsList posts={this.props.posts} />      
-                      <Form />
-               </div>
+                 <div className="row">
+                    <div className="col-md-6 col-md-offset-3">
+                       <div className="page-header">
+                          <h1>Hacker News</h1>
+                             <NewsList posts={posts} 
+                                  upvoteHandler={this.incrementUpvote} />
+                             <Form />
+                       </div>
+                    </div>
                  </div>
-                </div>
               </div>
           );
-      }
+      } 
     }
 
     export default HackerApp;
---
+++ src/test/stubAPI.js
@@ -0,0 +1,104 @@
+ import _ from 'lodash';
+
+    class StubAPI {
+
+        constructor() {
+            this.posts = [
+                {  id: 1 ,
+                    title : 'India - Tiger population sees 30% increase.',
+                    link : 'http://www.bbc.com/news/world-asia-30896028',
+                    username : 'jbloggs',
+                    comments : [],
+                    upvotes : 10
+                },
+                { 
+                    id: 2,
+                    title : 'The button that is not.',
+                    link : 'http://blog.nuclearsecrecy.com/2014/12/15/button-isnt/',
+                    username : 'notme',
+                    comments : [],
+                    upvotes : 12
+                },
+                { 
+                    id: 3,
+                    title : 'Google Nears $1B Investment in SpaceX',
+                    link : null,
+                    username : 'notme',
+                    comments : [],
+                    upvotes : 12
+                },
+                { 
+                    id: 4,
+                    title : 'Coinbase Raises $75M from DFJ Growth, USAA, and More',
+                    link : 'http://blog.coinbase.com/post/108642362357/coinbase-raises-75m-from-dfj-growth-usaa-nyse',
+                    username : 'psmith',  
+                    comments : [],
+                    upvotes : 2
+                }
+            ] ;
+        }
+
+        getAll() {
+            return this.posts ;
+        }
+
+        add(t,l) {
+            let id = 1 ;
+            let last = _.last(this.posts) ;
+            if (last) {
+                id = last.id + 1 ;
+            }
+            let len = this.posts.length ;
+            let newLen = this.posts.push({ 
+                'id': id,  
+                title: t, link : l, username: '', comments: [], upvotes: 0 }) ;
+            return newLen > len ;
+        }
+
+        upvote(id) {
+            let index = _.findIndex(this.posts, 
+                function(post) {
+                    return post.id === id;
+                } );   
+            if (index !== -1) {                 
+                this.posts[index].upvotes += 1 ;
+                return true ;
+            }
+            return false ;
+        }
+
+        getPost(id) {
+            let result = null ;
+            let index = _.findIndex(this.posts, function(post) {
+                return post.id === id;
+            } );     
+            if (index !== -1) {                 
+                result = this.posts[index];
+            }
+            return result ;
+        }
+
+        addComment(postId,c,n) {
+            let post = this.getPost(postId ) ;
+            let id = 1 ;
+            let last = _.last(post.comments) ;
+            if (last) {
+                id = last.id + 1 ;
+            }
+            post.comments.push({ 'id': id,  
+                comment: c , author: n, upvotes: 0 } ) ;
+        }
+
+        upvoteComment(postId,commentId) {
+            let post = this.getPost(postId ) ;
+            let index = _.findIndex(post.comments, function(c) {
+                return c.id === commentId;
+            } );  
+            if (index !== -1) {                 
+                post.comments[index].upvotes += 1 ;
+            }
+
+        }
+    }
+
+    export default (new StubAPI() );
